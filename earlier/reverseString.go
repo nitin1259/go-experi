628e5c0aca14bc0ad4166d993b44b83e7692c23d
nisingh <nisingh@egnyte.com>
2023-08-23 07:35:45 +0530

    example of reverse string

---
+++ earlier/reverseString.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("reverse string")
+
+	fmt.Println("Reverse String: ", reverseString("Kapila pashu Aahar"))
+
+	fmt.Println("Reverse String: ", reverseStr("Kapilap"))
+
+}
+
+func reverseString(str string) string {
+	if len(str) <= 1 {
+		return str
+	}
+
+	return reverseString(str[1:]) + string(str[0])
+}
+
+func reverseStr(str string) string {
+	rns := []rune(str)
+
+	for i := 0; i < len(rns)/2; i++ {
+		rns[i], rns[len(rns)-i-1] = rns[len(rns)-i-1], rns[i]
+	}
+
+	return string(rns)
+}
