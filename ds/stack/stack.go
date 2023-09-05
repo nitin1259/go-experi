b3298c6162657b4d6bac2e69ff3d1722401ad3e4
nisingh <nisingh@egnyte.com>
2023-09-05 23:29:10 +0530

    Stack Implementation

---
+++ ds/stack/stack.go
@@ -0,0 +1,39 @@
+package stack
+
+import "sync"
+
+type Stack[T any] struct {
+	arr  []T
+	lock sync.RWMutex
+}
+
+func (s *Stack[T]) Push(in T) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.arr = append(s.arr, in)
+}
+
+func (s *Stack[T]) Pop() (out T) {
+	if s.IsEmpty() {
+		return
+	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	out = s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return out
+}
+
+func (s *Stack[T]) Top() (out T) {
+	if s.IsEmpty() {
+		return
+	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	out = s.arr[len(s.arr)-1]
+	return out
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.arr) == 0
+}
