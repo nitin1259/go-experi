aad583526c4dfe2ee5a082670f12152d8a036bd8
nisingh <nisingh@egnyte.com>
2024-01-18 09:55:26 +0530

    New program added

---
+++ java_odds/Pragmaticpaly.java
@@ -0,0 +1,46 @@
+package com.renovsys;
+
+import java.util.LinkedList;
+
+// print 1 to 100 with 4 threads in sequence 
+
+public class TestPragmaticplay {
+
+    private static final int Max_Count = 100;
+    static int count = 1;
+
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(()-> printNumbers(1));
+        Thread t2 = new Thread(()-> printNumbers(2));
+        Thread t3 = new Thread(()-> printNumbers(3));
+        Thread t4 = new Thread(()-> printNumbers(4));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+
+
+    private static void printNumbers(int i){
+        synchronized (lock) {
+            while (count < Max_Count) {
+                if (count % 4 == (i - 1)) {
+                    System.out.println("Thread Name: "+ i +", count:"+ count);
+                    count++;
+                    lock.notifyAll();
+                    ;
+                } else {
+                    try {
+                        lock.wait();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
