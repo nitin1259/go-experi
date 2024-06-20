801b6960b42dbd1173e7f02b8d261abffc56f9c6
nisingh <nisingh@egnyte.com>
2024-06-20 16:01:10 +0530

    interview question - samsung

---
+++ 2024/goques/samsung_round1.go
@@ -0,0 +1,18 @@
+package goques
+
+/*
+
+Given an array of integers (not necessarily distinct) A, if we split the array into some number of "chunks" (partitions), and individually sort each chunk. After concatenating them, the result equals the sorted array. What is the most number of chunks we could have made?
+
+input -> [2, 0, 1, 2]
+output-> [2,0,1], [2] -> [0,1,2,2]
+
+Number of chunks: 2
+
+*/
+
+import "fmt"
+
+func program(){
+	fmt.Println("starting program")
+}
