d61ccd18bab2a9433c9861d391e16024d06dc96b
nisingh <nisingh@egnyte.com>
2024-07-25 11:01:20 +0530

    ds : linked list reverse func

--- ds/linkedlist/linkedNode.go
+++ ds/linkedlist/linkedNode.go
@@ -4,3 +4,17 @@ type LinkNode[T any] struct {
 	Val  T
 	Next *LinkNode[T]
 }
+
+func ReverseLinkedList(head *LinkNode[int])*LinkNode[int]{
+
+		var curr, prev *LinkNode[int]
+		curr = head
+
+	 for curr!=nil{
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	 }
+	 return prev
+}
